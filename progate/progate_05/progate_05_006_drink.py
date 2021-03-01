548e39b97bc40d65268ff1d294a389110d932866
reinaaa <Reinaaa0555@gmail.com>
2021-03-01 23:38:38 +0900

    pythonの演習問題

---
+++ progate/progate_05/progate_05_006_drink.py
@@ -0,0 +1,6 @@
+from menu_item import MenuItem
+
+class Drink(MenuItem):
+    # infoメソッドを定義してください
+    def info(self):
+        return self.name + ': ¥' + str(self.price) + ' (' + str(self.amount) + 'mL)'
