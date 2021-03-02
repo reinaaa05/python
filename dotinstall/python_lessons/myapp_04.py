bf194c09646832e5d8372f807b8d5e5e71773121
reinaaa <Reinaaa0555@gmail.com>
2021-03-02 23:39:57 +0900

    pythonの演習問題

---
+++ dotinstall/python_lessons/myapp_04.py
@@ -0,0 +1,12 @@
+#文字列
+s = "he\nllo wor\tld"
+html = """<html>
+<body></body>
+</html>"""
+print(s)
+print(html)
+
+#数値
+i = 10
+
+
---
+++ dotinstall/python_lessons/myapp_06.py
@@ -0,0 +1,6 @@
+name = "taguchi"
+score = 52.8
+
+#print("name: %s, score: %f" %(name, score))
+#print("name: %-10s, score: %10.2f" %(name, score))
+print("name: {0:>10s}, score: {1:<10.2f}".format(name, score))
