a11b8d07a5aaec1caad60fc391ac8fa9e494dbe6
reinaaa <Reinaaa0555@gmail.com>
2021-05-11 14:33:35 +0900

    pythonの基礎

---
+++ udemy/basic/class/basic9.py
@@ -0,0 +1,40 @@
+#ポリモーフィズム
+
+from abc import abstractmethod, ABCMeta
+
+class Human(metaclass=ABCMeta): #親クラス
+
+    def __init__(self,name):
+        self.name = name
+    
+    @abstractmethod
+    def say_something(self):
+        pass
+
+    def say_name(self):
+        print(self.name)
+
+class Woman(Human):
+
+    def say_something(self):
+        print('女性: 名前は={}'.format(self.name))
+
+
+class Man(Human):
+
+    def say_something(self):
+        print('男性: 名前は={}'.format(self.name))
+
+
+human = Woman('Hanako')
+
+
+#ポリモーフィズム
+num = input('0か1を入力してください')
+if num == '0':
+    human = Man('Taro')
+elif num == '1':
+    human = Woman('Hanako')
+else:
+    print('間違っています')
+human.say_something()
---
+++ udemy/basic/excersize/basic5.py
@@ -0,0 +1,38 @@
+from abc import abstractclassmethod, ABCMeta
+
+class Animals(metaclass=ABCMeta):
+
+    @abstractclassmethod
+    def speak(self):
+        pass
+
+class Dog(Animals):
+
+    def speak(self):
+        print('わん')
+
+class Cat(Animals):
+
+    def speak(self):
+        print('にゃー')
+
+class Sheep(Animals):
+
+    def speak(self):
+        print('メー')
+
+class Other(Animals):
+    def speak(self):
+        print('そんな動物いないよ')
+
+number = input('好きな動物は？ 1:犬, 2:猫, 3:羊')
+
+if number == '1':
+    animal = Dog()
+elif number == '2':
+    animal = Cat()
+elif number == '3':
+    animal = Sheep()
+else: 
+    animal = Other()
+animal.speak() #speakの呼び出し
